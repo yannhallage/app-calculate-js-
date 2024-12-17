6af6e105259216e0d44662c1a12bab4aa8b02819
yannhallage <contact.devhllg@gmail.com>
2024-12-17 03:19:52 +0000

    modifie index.js et index.html

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express()
 const path = require('path')
 const port = 3000
 
+// autoriser les files statiques comme les files css 
+// app.use(express.static('public'))
+ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })

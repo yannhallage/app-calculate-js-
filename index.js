671625659c23716a8d3ee4c02a89fbe2eaf27861
yannhallage <contact.devhllg@gmail.com>
2024-12-17 03:00:59 +0000

    premiere modife de index

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/home', (req, res) => {
+  res.send('welcome yann hallage')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

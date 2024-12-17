214e2e68d8999c785b50b529ac3e2193acfa5f79
yannhallage <contact.devhllg@gmail.com>
2024-12-17 03:09:13 +0000

    prise en forme du sendfile

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/home', (req, res) => {
-  res.send('welcome yann hallage')
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(port, () => {

9649ba0ca5e8c09163d7ac48635598a55806a40a
yannhallage <contact.devhllg@gmail.com>
2024-12-17 03:22:52 +0000

    mise a jour

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const port = 3000
 // app.use(express.static('public'))
  app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
